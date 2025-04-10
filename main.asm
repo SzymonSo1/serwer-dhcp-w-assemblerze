45d354e328e174229f3fa6a6b4a130fec721dd92
DelusionalMikuFan <97904365+SzymonSo1@users.noreply.github.com>
2025-04-10 19:43:03 +0200

    Update main.asm

--- main.asm
+++ main.asm
@@ -2,7 +2,7 @@ global _start
 
 section .data
 
-msg_D db "Otrzymalem dhcp discover", 0xA,0x0
+msg_D db "Otrzymalem dhcp discover", 0xA, 0x0
 len_msgD equ $-msg_D
 
 msg_O db "Wyslalem dhcp offer", 0xA, 0x0
@@ -31,6 +31,15 @@ section .bss
 fd: resd 1 
 recbuff: resq 256
 sendbuff: resq 256
+
+clientadr:
+dw 0x2
+dw 0x4301
+db 0x0
+db 0x0
+db 0x0
+db 0x0
+dq 0x0
 
 section .text
 ;rdi rsi rdx r10 r8 r9
@@ -82,15 +91,32 @@ mov byte rax, [recbuff+rcx]
 cmp rax, 0x3
 call print_req
 
+;mov byte al, [recbuff+2]
+;cmp rax, 0x6
+;jne main_loop
+;mov rax, 1
+;mov rdi, 1
+;mov rsi, msg_D
+;mov rdx, len_msgD
+;syscall
 
-mov byte al, [recbuff+2]
-cmp rax, 0x6
-jne main_loop
-mov rax, 1
-mov rdi, 1
-mov rsi, msg_D
-mov rdx, len_msgD
-syscall
+sending:
+mov byte [sendbuff], 0x2			; opcode 1,2 (req,res)
+mov byte [sendbuff+1], 0x1			; hw type (eth 1)
+mov byte [sendbuff+2], 0x6			; hw length (6 mac)
+mov byte [sendbuff+3], 0x0			; hops 0
+mov word [sendbuff+4], [recbuff+4]		; transaction id
+mov word [sendbuff+8], 0x0			; jakies sekundy
+mov word [sendbuff+10], 0x0			; boottp flags unicast - 0
+mov dword [sendbuff+12], [recbuff+12]		; adres klienta
+mov dword [sendbuff+16], [sendbuff+12]		; nowy adres
+mov dword [sendbuff+20], [adr+4]		; adres serwera
+mov dword [sendbuff+24], 0x0			; relay adres 
+mov dword [sendbuff+28], [recbuff+28]		; mac adres
+mov word [sendbuff+32], [recbuff+32]		; mac adres cz 2
+mov qword [sendbuff+34], 0x0			; padding mac
+mov word [sendbuff+42], 0x0			; padding mac cz 2
+
 
 jmp main_loop
 
