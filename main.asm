a3671887c0a7d2f258028f7598ee5307465e86da
DelusionalMikuFan <97904365+SzymonSo1@users.noreply.github.com>
2025-04-10 21:52:35 +0200

    Update main.asm

--- main.asm
+++ main.asm
@@ -33,13 +33,10 @@ recbuff: resq 256
 sendbuff: resq 256
 
 clientadr:
-dw 0x2
-dw 0x4301
-db 0x0
-db 0x0
-db 0x0
-db 0x0
-dq 0x0
+resw 1
+resw 1
+resd 1
+resq 1
 
 section .text
 ;rdi rsi rdx r10 r8 r9
@@ -74,6 +71,11 @@ mov rsi, recbuff
 mov rdx, 1500
 syscall
 
+check_for_opcode:
+mov byte al, [recbuff]
+cmp rax, 0x1
+jne main_loop
+
 read_dhcp_type:
 push rcx
 mov rax, 0
@@ -91,45 +93,59 @@ mov byte rax, [recbuff+rcx]
 cmp rax, 0x3
 call print_req
 
-;mov byte al, [recbuff+2]
-;cmp rax, 0x6
-;jne main_loop
-;mov rax, 1
-;mov rdi, 1
-;mov rsi, msg_D
-;mov rdx, len_msgD
-;syscall
+mov word [clientadr], 0x2
+mov word [clientadr+2], 0x4400
+mov dword [clientadr+4], 0x0100007F
+mov qword [clientadr+8], 0x0
 
 tworzenie_pakietu:
+xor rax, rax
 mov byte [sendbuff], 0x2			; opcode 1,2 (req,res)
 mov byte [sendbuff+1], 0x1			; hw type (eth 1)
 mov byte [sendbuff+2], 0x6			; hw length (6 mac)
 mov byte [sendbuff+3], 0x0			; hops 0
-mov word [sendbuff+4], [recbuff+4]		; transaction id
+mov dword eax, [recbuff+4]
+mov dword [sendbuff+4], eax			; transaction id
+xor eax, eax
 mov word [sendbuff+8], 0x0			; jakies sekundy
 mov word [sendbuff+10], 0x0			; boottp flags unicast - 0
-mov dword [sendbuff+12], [recbuff+12]		; adres klienta
-mov dword [sendbuff+16], [sendbuff+12]		; nowy adres
-mov dword [sendbuff+20], [adr+4]		; adres serwera
-mov dword [sendbuff+24], 0x0			; relay adres 
-mov dword [sendbuff+28], [recbuff+28]		; mac adres
-mov word [sendbuff+32], [recbuff+32]		; mac adres cz 2
+mov dword eax, [recbuff+12]
+mov dword [sendbuff+12], eax			; adres klienta
+mov dword [sendbuff+16], eax			; nowy adres
+mov dword eax, [adr+4]
+mov dword [sendbuff+20], eax			; adres serwera
+;xor eax, eax
+mov dword [sendbuff+24], 0x0			; relay adres
+mov dword eax, [recbuff+28]
+mov dword [sendbuff+28], eax			; mac adres
+xor eax, eax
+mov word ax, [recbuff+32]
+mov word [sendbuff+32], ax			; mac adres cz 2
+xor ax, ax
 mov qword [sendbuff+34], 0x0			; padding mac
 mov word [sendbuff+42], 0x0			; padding mac cz 2
-mov dword [sendbuff+236], 0x63825363		; ciasteczko dhcp
-mov word [sendbuff+240], 0x3501			; opcja 53 dlugosc 1
+mov dword [sendbuff+236], 0x63538263		; ciasteczko dhcp
+mov word [sendbuff+240], 0x0135			; opcja 53 dlugosc 1
 mov byte [sendbuff+242], 0x05			; 5 - dhcpack
-mov word [sendbuff+243], 0x0104			; maska podsieci, dlugosc opcji
-mov dword [sendbuff+245], 0xffffff00		; 
-mov word [sendbuff+249], 0x3304			; ip lease time
-mov word [sendbuff+251], 0x3840			; czas 
-mov word [sendbuff+253], 0x3601			; ip servera
-mov dword [sendbuff+255], [adr+4]		;
-mov byte [sendbuff+259], 0xFF			; koniec opcji,pakietu
+mov word [sendbuff+243], 0x0401			; maska podsieci, dlugosc opcji
+mov dword [sendbuff+245], 0x00ffffff		; 
+mov word [sendbuff+249], 0x0433			; ip lease time
+mov dword [sendbuff+251], 0x40380000			; czas 
+mov word [sendbuff+255], 0x0436			; ip servera
+mov dword eax, [adr+4]
+mov dword [sendbuff+257], eax			;
+xor rax, rax
+mov byte [sendbuff+261], 0xFF			; koniec opcji,pakietu
 
 wysylanie:
-
-
+mov rax, 44
+mov rdi, [fd]
+mov rsi, sendbuff
+mov rdx, 262
+mov r10, 0
+mov r8, clientadr
+mov r9, 0x10
+syscall
 
 jmp main_loop
 
@@ -153,7 +169,7 @@ push rax
 mov rax,1
 mov rdi, 1
 mov rsi, msg_R
-mov rdx, len_msgR
+mov rdx, 25
 syscall
 pop rax
 ret
